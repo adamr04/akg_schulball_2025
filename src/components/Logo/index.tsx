31e30b353c799cfb1a5de709f6dab02f4e05cb7f
kaimbacher <alexander@kaimbacher.com>
2024-10-27 20:08:43 +0100

    45
    geändert auf 46

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ import "./Logo.styles.css";
 export const Logo: React.FC = () => {
   return (
     <div className="logo">
-      <em>45. Schulball</em>
+      <em>46. Schulball</em>
       <small>Akademisches Gymnasium Wien</small>
     </div>
   );
